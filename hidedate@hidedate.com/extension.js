56bf20151710f062969cd96fc37aeb7a5ab4a98a
Büşra <43908111+busracagliyan@users.noreply.github.com>
2022-07-25 15:53:56 +0300

    Update extension.js

--- hidedate@hidedate.com/extension.js
+++ hidedate@hidedate.com/extension.js
@@ -2,19 +2,12 @@ const Main = imports.ui.main;
 const Panel = Main.panel;
 
 let dateMenu = Panel.statusArea.dateMenu;
-let hiding;
 
 function enable() {
       dateMenu.container.hide();
-      hiding = dateMenu.actor.connect('notify::visible', actor => {
-        actor.hide()
-      })
 }
 
 function disable() {
-	if(hiding){
-		dateMenu.actor.disconnect(hiding);
-	}
     	dateMenu.container.show();
 }
 
